f5dde1789cd592438b07f976c3644610ea9e901d
Gaizka <gdiaz@nalej.com>
2018-10-23 10:51:18 +0200

    Removed vendor folder

--- vendor/github.com/nalej/example-grpc/README.md
+++
@@ -1,2 +0,0 @@
-# example-grpc
-Example project that defines a basic gRPC proto definition for Go
--- vendor/github.com/nalej/example-grpc/create_stubs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Generating gRPC stubs"
-
-for service_dir in $(ls -d */); do
-    SERVICE=${service_dir%%/};
-    echo "Generating protobuf for ${SERVICE}"
-    protoc -I ${SERVICE}/ ${SERVICE}/*.proto --go_out=plugins=grpc:${SERVICE}
-done
